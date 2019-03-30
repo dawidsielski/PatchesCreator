cb0ed430cd14dd4efff2564feb4a7d9e8fa9089c
turbodivx <dawid.sielski@outlook.com>
2019-03-30 08:54:51 +0100

    Patches size and patches number in init.

--- PatchesCreation.py
+++ PatchesCreation.py
@@ -11,13 +11,17 @@ NUMBER_OF_PATCHES = 60
 
 class PatchesCreation(object):
 
+    def __init__(self, patches_size=32, patches=60):
+        self.patches_size = patches_size
+        self.patches = patches
+
     @staticmethod
     def check_entropy():
         pass
 
     @staticmethod
-    def generate_new_position(patches_size, size):
-        upper_bound, lower_bound = patches_size, size[0] - patches_size
+    def generate_new_position(patches_size, upper_bound_img_size):
+        upper_bound, lower_bound = patches_size, upper_bound_img_size - patches_size
         x, y = random.randint(upper_bound, lower_bound), random.randint(upper_bound, lower_bound)
         return x, y
 
@@ -37,7 +41,7 @@ class PatchesCreation(object):
 
         i = 0
         while i < NUMBER_OF_PATCHES:
-            x, y = PatchesCreation.generate_new_position(patches_size, img.shape)
+            x, y = PatchesCreation.generate_new_position(patches_size, img.shape[0])
             patch = img[x: x + patches_size, y: y + patches_size]
 
             cv2.imwrite(os.path.join(new_dir, '{}_{}_{}.png'.format(cartoon_number, x, y)), patch)
